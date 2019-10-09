d055c96ede7f8e825a94460ef29a25b64547ea17
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-09 21:42:10 +0700

    Merubah directory folder part1 menjadi part2, serta menambahkan file produk.php

---
+++ part3/produk.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Produk {
+	public $judul = "judul", 
+		   $penulis = "penulis",
+		   $penerbit = "penerbit",
+		   $harga = 0;
+
+	public function getLable(){
+		return "$this->penulis, $this->penerbit";
+	}
+}
+
+
+$produk1 = new produk();
+$produk1->judul = "Naruto";
+$produk1->penulis = "Dicky Ardianto";
+$produk1->penerbit = "Sandika";
+$produk1->harga = 10000;
+
+
+$produk2 = new Produk();
+$produk2->judul = "Call Of Duty";
+$produk2->penulis = "Gajah Mada";
+$produk2->penerbit = "Bakso Tahu";
+$produk2->harga = 2000000;
+
+echo " Komik : " . $produk1->getLable();
+echo "<hr>";
+echo "Game : " . $produk2->getLable();
+
+ ?>
